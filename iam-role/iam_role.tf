231ee28d94fbc1eeeaed7d612973204507fa7576
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-08 08:05:07 -0800

    fix: update outputs (#32)
    fix: correct outputs in module

--- iam-role/iam_role.tf
+++ iam-role/iam_role.tf
@@ -1,9 +1,13 @@
-module "ecs_access" {
+locals {
+  iam_role_name   = "${var.prefix}-nuon-${var.sandbox}-access"
+}
+
+module "iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = ">= 5.1.0"
 
   create_role = true
-  role_name   = "${var.prefix}-nuon-${var.sandbox}-access"
+  role_name   = local.iam_role_name
 
   allow_self_assume_role   = true
   custom_role_trust_policy        = file("${path.module}/../${var.sandbox}/artifacts/trust.json")
