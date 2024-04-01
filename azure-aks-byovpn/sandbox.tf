f6d6ba63b33649709953532825bad589f5147f78
Jordan Acosta <jordan@nuon.co>
2024-04-01 10:43:16 -0700

    fix(azure-aks-byovpn): use newly-published sandbox module (#72)

--- azure-aks-byovpn/sandbox.tf
+++ azure-aks-byovpn/sandbox.tf
@@ -1,7 +1,6 @@
 module "sandbox" {
-  # source  = "nuonco/aks-byovpn-sandbox/azure"
-  # version = "1.0.0"
-  source = "../../terraform-azure-aks-byovpn-sandbox"
+  source  = "nuonco/aks-byovpn-sandbox/azure"
+  version = "1.0.2"
 
   network_name        = var.network_name
   resource_group_name = var.resource_group_name
