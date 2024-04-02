239c1fa36334d011756604d04d078daef4988862
Jordan Acosta <jordan@nuon.co>
2024-04-01 19:05:15 -0700

    chore(azure-aks-byovpn): use v1.0.4 of sandbox module (#73)

--- azure-aks-byovpn/sandbox.tf
+++ azure-aks-byovpn/sandbox.tf
@@ -1,6 +1,6 @@
 module "sandbox" {
   source  = "nuonco/aks-byovpn-sandbox/azure"
-  version = "1.0.2"
+  version = "1.0.4"
 
   network_name        = var.network_name
   resource_group_name = var.resource_group_name
