9bbfe859a080dea0f49c71bd42327a985c0b2ad8
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-17 21:31:51 -0600

    chore: update aks sandbox (#87)

--- azure-aks-byovpn/sandbox.tf
+++ azure-aks-byovpn/sandbox.tf
@@ -1,6 +1,6 @@
 module "sandbox" {
   source  = "nuonco/aks-byovpn-sandbox/azure"
-  version = "v1.2.7"
+  version = "v1.2.8"
 
   network_name        = var.network_name
   resource_group_name = var.resource_group_name
