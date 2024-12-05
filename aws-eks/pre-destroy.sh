fd10f45b2ef71a51b509cb22384f99f77387ea78
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-05 16:45:59 -0600

    fix: aws-eks: make pre-destroy-a-noop (#145)

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -4,13 +4,4 @@ set -u
 set -o pipefail
 
 
-echo "executing error-destroy script"
-echo
-echo '     region: '$AWS_REGION
-echo '    profile: '$AWS_PROFILE
-echo ' install id: '$NUON_INSTALL_ID
-echo
-
-
-echo "list deployments in install namespace"
-kubectl -n $NUON_INSTALL_ID get deployments -o json
+echo "executing error-destroy script: noop"
