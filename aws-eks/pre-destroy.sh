73a9b67c04fe36bc8d5efc945fe8d68cf2ea6ed7
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-09 10:27:31 -0600

    chore: aws-eks/pre-destroy.sh: handle case where ns dne (#150)
    it is possible this script may run twice so we must be prepared to
    handle the case where the namespace no longer exists.

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -2,8 +2,9 @@
 
 set -u
 set -o pipefail
+set -e
 
 
 echo "executing error-destroy script"
 aws eks update-kubeconfig --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID
-kubectl delete namespace $NUON_INSTALL_ID
+kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
