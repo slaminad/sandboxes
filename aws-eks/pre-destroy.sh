3105f829f6f2c81814a862c3b778640161889081
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-09 09:35:49 -0600

    feat: aws-eks: pre-destroy script deletes the install namespace (#149)

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -4,4 +4,6 @@ set -u
 set -o pipefail
 
 
-echo "executing error-destroy script: noop"
+echo "executing error-destroy script"
+aws eks update-kubeconfig --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID
+kubectl delete namespace $NUON_INSTALL_ID
