4f834e9f4ca10f2ecd1bffa9675d4d47cf16e0b5
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-10 13:50:08 -0600

    chore: aws-eks: pre destroy script logging improvements (#152)
    * chore: aws-eks: pre-destroy - exit quietly if unable to get credentials
    
    * chore: aws-eks: pre-destroy - exit quietly if unable to get credentials

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -4,7 +4,11 @@ set -u
 set -o pipefail
 set -e
 
+echo "[pre-destroy] preparing to execute pre-destroy script"
+aws sts get-caller-identity
 
-echo "executing error-destroy script"
-aws eks update-kubeconfig --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID
+echo "[pre-destroy] getting cluster credentials with aws eks update-kubeconfig"
+aws eks update-kubeconfig --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || echo "[pre-destroy] unable to get k8s credentials - exiting gracefully" exit 0
+
+echo "[pre-destroy] deleting install namespace"
 kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
