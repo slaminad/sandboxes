32e6f7efaeeb01de6d6939d50582228f19eacba4
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-10 14:45:21 -0600

    fix: aws-eks: pre-destroy set kubectl context (#157)

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -12,4 +12,5 @@ echo "[pre-destroy] getting cluster credentials with aws eks update-kubeconfig"
 aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || exit 0
 
 echo "[pre-destroy] deleting install namespace"
+kubectl config set-context $NUON_INSTALL_ID
 kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
