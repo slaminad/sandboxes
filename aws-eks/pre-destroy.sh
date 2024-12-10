ee4d07e346ad0389b8c5dccb4228c2b96e019cb9
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-10 14:15:27 -0600

    chore: aws-eks: pre-destroy script graceful exit (#154)
    chore: aws-eks: pre-destroy - graceful exit

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 set -u
-set -o pipefail
 set -e
 
 echo "[pre-destroy] preparing to execute pre-destroy script"
 role=aws sts get-caller-identity | jq '.Arn'
 
-echo "[pre-destroy] running as role: "$role
+echo "[pre-destroy] region: "$AWS_REGION" role: "$role
 
 echo "[pre-destroy] getting cluster credentials with aws eks update-kubeconfig"
-aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || echo "[pre-destroy] unable to get k8s credentials - exiting gracefully" && exit 0
+aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || exit 1
 
 echo "[pre-destroy] deleting install namespace"
 kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
