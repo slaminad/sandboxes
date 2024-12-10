b6a634b516f7dd8ea641979581fcf907e4a89047
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-10 14:44:08 -0600

    fix: aws-eks: pre-destroy exit code (#156)

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -9,7 +9,7 @@ role=`aws sts get-caller-identity | jq -r '.Arn'`
 echo "[pre-destroy] region: "$AWS_REGION" role: "$role
 
 echo "[pre-destroy] getting cluster credentials with aws eks update-kubeconfig"
-aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || exit 1
+aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || exit 0
 
 echo "[pre-destroy] deleting install namespace"
 kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
