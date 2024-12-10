39de3adae74c471c28735db9f4148bdcfd4701da
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-10 14:05:44 -0600

    chore: aws-eks: pre-destroy script should use region when getting cluster details (#153)

--- aws-eks/pre-destroy.sh
+++ aws-eks/pre-destroy.sh
@@ -5,10 +5,12 @@ set -o pipefail
 set -e
 
 echo "[pre-destroy] preparing to execute pre-destroy script"
-aws sts get-caller-identity
+role=aws sts get-caller-identity | jq '.Arn'
+
+echo "[pre-destroy] running as role: "$role
 
 echo "[pre-destroy] getting cluster credentials with aws eks update-kubeconfig"
-aws eks update-kubeconfig --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || echo "[pre-destroy] unable to get k8s credentials - exiting gracefully" exit 0
+aws eks update-kubeconfig --region $AWS_REGION --name $NUON_INSTALL_ID --alias $NUON_INSTALL_ID || echo "[pre-destroy] unable to get k8s credentials - exiting gracefully" && exit 0
 
 echo "[pre-destroy] deleting install namespace"
 kubectl delete namespace $NUON_INSTALL_ID --ignore-not-found=true
