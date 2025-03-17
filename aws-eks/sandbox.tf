b8124ea156298644b0372e1e100568fe34236c59
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-03-17 17:38:12 -0500

    chore: aws-eks: use latest (#166)

--- aws-eks/sandbox.tf
+++ aws-eks/sandbox.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   # NOTE(fd): example format for testing branches
   # source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=e4c8e0feda3b80c84bb32a170f8969194160b621"
   source  = "nuonco/eks-sandbox/aws"
-  version = "1.4.7"
+  version = "1.5.1"
 
   install_name          = var.install_name
   cluster_name          = var.cluster_name
