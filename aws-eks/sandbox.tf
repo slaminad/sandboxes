5a0af5746d78d204c5bc798ee829862dcf3388e2
Dan Merino <dcm@retool.com>
2025-04-08 15:45:59 -0700

    add extra vpc configuration

--- aws-eks/sandbox.tf
+++ aws-eks/sandbox.tf
@@ -1,8 +1,11 @@
 module "sandbox" {
   # NOTE(fd): example format for testing branches
   # source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=e4c8e0feda3b80c84bb32a170f8969194160b621"
-  source  = "nuonco/eks-sandbox/aws"
-  version = "1.6.1"
+  #source  = "nuonco/eks-sandbox/aws"
+  source = "github.com/slaminad/terraform-aws-eks-sandbox"
+  #version = "1.6.1"
+
+  number_azs = 3
 
   install_name          = var.install_name
   cluster_name          = var.cluster_name
